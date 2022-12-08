bdde385d592a38ec2e5b7759e7b6ffaafa2fee26
VLola <valiklolanew@gmail.com>
2022-12-09 00:02:33 +0200

    added task 23

---
+++ Task_23/src/App/App.js
@@ -0,0 +1,12 @@
+import logo from '../images/LolaValik.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img className='logo' src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
